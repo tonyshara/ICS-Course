1a53c280f139e034f197f875f6b92af24a523e92
tonyshara <tony.shara@icloud.com>
2017-05-29 19:06:59 -0700

    week 3 hw

---
+++ ch5/biggerBetterFavoriteNumber.rb
@@ -0,0 +1,4 @@
+puts 'What is your favorite number?'
+fav = gets.to_i
+nfav = fav+1
+puts 'I think that ' + nfav.to_s + ' is a bigger, better favorite number.'
---
+++ ch5/fullNameGreeting.rb
@@ -0,0 +1,7 @@
+puts 'What is your first name?'
+first = gets.chomp
+puts 'What is your middle name?'
+middle = gets.chomp
+puts 'What is your last name?'
+last = gets.chomp
+puts 'Hello ' + first + ' ' + middle + ' ' + last + ', how are you doing today?'
