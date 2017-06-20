566defa92d793c9a6a1fb2a30d23326db9fdaae8
tonyshara <tony.shara@icloud.com>
2017-06-20 14:55:20 -0700

    fix grandma extended - 'bye' counter

--- ch07/deafGrandmaExtended.rb
+++ ch07/deafGrandmaExtended.rb
@@ -1,23 +1,31 @@
-say = gets.chomp 
+byecount = 1
+say = gets.chomp
 while true
-  if say != say.upcase
-  puts 'HUH?! SPEAK UP, SONNY!'
-  say = gets.chomp
-  elsif say == say.upcase && say == 'BYE'
-    byecount = 1
-    if say == 'BYE'
-      while byecount < 3
-      byecount += 1
-      puts '...'
-      say = gets.chomp
-      if byecount == 3
-        puts 'Good bye'
-        break
-        end
-      else
-      year = rand(1930..1951).to_s
-      puts 'No, not since ' + year + '!'
-    end
+  if say == say.upcase && say != 'BYE'
+    year = rand(1930..1951).to_s
+    puts 'No, not since ' + year + '!'
     say = gets.chomp
+  elsif say == say.downcase
+    puts 'HUH?! SPEAK UP, SONNY!'
+    say = gets.chomp
+  elsif say == 'BYE'
+    puts byecount
+    if byecount < 3
+      say = gets.chomp
+      if say == 'BYE'
+        byecount += 1
+      elsif say == say.upcase && say != 'BYE'
+        year = rand(1930..1951).to_s
+        puts 'No, not since ' + year + '!'
+        byecount = 1
+      else
+        say != say.upcase
+        puts 'HUH?! SPEAK UP, SONNY!'
+        byecount = 1
+      end
+    else
+      puts 'Good Bye!'
+      break
+    end
   end
 end
