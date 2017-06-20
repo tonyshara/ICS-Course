63ea1f5358f3e9cafa64494b7e66e850f9a3e409
tonyshara <tony.shara@icloud.com>
2017-06-20 15:17:09 -0700

    change lists to chapter, title, and page number

--- ch08/tableOfContentsRevisited.rb
+++ ch08/tableOfContentsRevisited.rb
@@ -1,12 +1,12 @@
 line_width = 30
-puts 'Table of contents'.center(50)
-ch1 = ['1', 'Getting Started', '1']
-puts 'Chapter ' + ch1[0] + ': ' + ch1[1] +  '           page  ' + ch1[2].ljust(line_width)
-ch2 = ['2', 'Numbers', '9']
-puts 'Chapter ' + ch2[0] + ': ' + ch2[1] + '                   page  ' + ch2[2].ljust(line_width)
-ch3 = ['3', 'Letters', '13']
-puts 'Chapter ' + ch3[0] + ': ' + ch3[1] + '                   page ' + ch3[2].ljust(line_width)
+ch = ['1', '2', '3']
+ti = ['Getting Started', 'Numbers', 'Letters']
+pg = ['1', '9', '13']
 
-#puts 'Chapter 1: Getting started         page  1'.ljust(line_width)
-#puts 'Chapter 2: Numbers                 page  9'.ljust(line_width)
-#puts 'Chapter 3: Letters                 page 13'.ljust(line_width)
+puts 'Table of contents'.center(50)
+#ch1 = ['1', 'Getting Started', '1']
+puts 'Chapter ' + ch[0] + ': ' + ti[0] +  '           page  ' + pg[0].ljust(line_width)
+#ch2 = ['2', 'Numbers', '9']
+puts 'Chapter ' + ch[1] + ': ' + ti[1] + '                   page  ' + pg[1].ljust(line_width)
+#ch3 = ['3', 'Letters', '13']
+puts 'Chapter ' + ch[2] + ': ' + ti[2] + '                   page ' + pg[2].ljust(line_width)
