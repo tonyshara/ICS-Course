e168dd8a91dc75da7b971e1b69d92d5a41fbd908
tonyshara <tony.shara@icloud.com>
2017-06-22 09:41:24 +0200

    week  5 hw :ch9 - improved ask method

---
+++ ch09/improvedAskMethod.rb
@@ -0,0 +1,19 @@
+def ask question
+  while true
+    puts question
+    reply = gets.chomp.downcase
+    if (reply == 'yes' || reply == 'no')
+      if reply == 'yes'
+        return true
+      else
+        return false
+      end
+      break
+    else
+      puts 'Please answer "yes" or "no".'
+    end
+  end
+end
+
+maybe = ask 'Can you answer my question?'
+puts maybe
